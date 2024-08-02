54391286f9cf5c8ae22e0c915d472334f5842c3b
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-08-02 18:45:14 +0200

    First commit

---
+++ 2.js
@@ -0,0 +1,15 @@
+'use strict'
+
+console.log(4 / 0);
+console.log(-44 / 0);
+console.log('str' * 10);
+const str = 'Alex';
+let str1;
+console.log(str1);
+
+const obj = {
+    name: 'John',
+    age: 25,
+}
+
+console.log(obj.name);
